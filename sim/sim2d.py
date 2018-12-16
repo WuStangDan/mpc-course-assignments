3a3636cc1e1b66fb319363406623e0da86ca7103
Daniel Stang <dstang14@gmail.com>
2018-12-15 16:41:09 -0800

    General cleanup.

--- sim/sim2d.py
+++ sim/sim2d.py
@@ -15,50 +15,50 @@ def sim_run(options, MPC):
 
     mpc = MPC()
 
-    prediction_horizon = 20
     num_inputs = 2
-    u = np.zeros(prediction_horizon*num_inputs)
+    u = np.zeros(mpc.horizon*num_inputs)
     bounds = []
 
-    #ref_1 = [10,10,0]
-    #ref_2 = [10, 2, 3.14159/2]
-    ref_1 = [10,0.01,0]
-    ref_2 = [10,0.01,0]
-    ref = ref_1
-
-    for i in range(prediction_horizon):
+    # Set bounds for inputs bounded optimization.
+    for i in range(mpc.horizon):
         bounds += [[-5, 5]]
         bounds += [[-0.8, 0.8]]
+
+    ref_1 = mpc.reference1
+    ref_2 = mpc.reference2
+    ref = ref_1
 
     state_i = np.array([[0,0,0,0]])
     u_i = np.array([[0,0]])
     sim_total = 250
     predict_info = [state_i]
 
+    sim_dt = 0.1
     for i in range(1,sim_total+1):
         u = np.delete(u,0)
         u = np.delete(u,0)
         u = np.append(u, u[-2])
         u = np.append(u, u[-2])
         start_time = time.time()
-        u_solution = minimize(mpc.cost_function, u, (prediction_horizon, state_i[-1], ref),
+
+        # Non-linear optimization.
+        u_solution = minimize(mpc.cost_function, u, (state_i[-1], ref),
                                 method='SLSQP',
                                 bounds=bounds,
                                 tol = 1e-5)
         print('Step ' + str(i) + ' of ' + str(sim_total) + '   Time ' + str(round(time.time() - start_time,5)))
         u = u_solution.x
-        y = mpc.plant_model(state_i[-1], 0.1, u[0], u[1])
-        if (i > 130):
+        y = mpc.plant_model(state_i[-1], sim_dt, u[0], u[1])
+        if (i > 130 and ref_2 != None):
             ref = ref_2
         predicted_state = np.array([y])
-        for j in range(1,prediction_horizon):
-            predicted = mpc.plant_model(predicted_state[-1], 0.1, u[2*j], u[2*j+1])
+        for j in range(1, mpc.horizon):
+            predicted = mpc.plant_model(predicted_state[-1], mpc.dt, u[2*j], u[2*j+1])
             predicted_state = np.append(predicted_state, np.array([predicted]), axis=0)
         predict_info += [predicted_state]
         state_i = np.append(state_i, np.array([y]), axis=0)
-        #print(u_i)
-        #print(np.array((u[0], u[1])))
         u_i = np.append(u_i, np.array([(u[0], u[1])]), axis=0)
+
 
     ###################
     # SIMULATOR DISPLAY
@@ -111,22 +111,6 @@ def sim_run(options, MPC):
         patch_obs = mpatches.Circle((mpc.x_obs, mpc.y_obs),0.5)
         ax.add_patch(patch_obs)
 
-    # V Estimate plot.
-    # ax2 = fig.add_subplot(gs[0:4, 4:])
-    # v_est, = ax2.plot([], [], '-b')
-    # plt.title('V Estimate')
-    # plt.xticks([])
-    # ax2.set_ylim([0,4])
-    # ax2.set_yticks([0,2,4])
-    #
-    # # X Estimate plot.
-    # ax3 = fig.add_subplot(gs[5:9, 4:])
-    # x_est, = ax3.plot([], [], '-b')
-    # plt.title('X Estimate Error')
-    # plt.xticks([])
-    # ax3.set_ylim(-4,4)
-    # ax3.set_yticks([-4,0,4])
-
     # Shift xy, centered on rear of car to rear left corner of car.
     def car_patch_pos(x, y, psi):
         #return [x,y]
@@ -154,7 +138,7 @@ def sim_run(options, MPC):
                         [telem[1]-2, telem[1]-2+max(0,-u_i[num,0]/5*4)])
 
         # Goal.
-        if (num <= 130):
+        if (num <= 130 or ref_2 == None):
             patch_goal.set_xy(car_patch_pos(ref_1[0],ref_1[1],ref_1[2]))
             patch_goal._angle = np.rad2deg(ref_1[2])-90
         else:
@@ -172,6 +156,6 @@ def sim_run(options, MPC):
     print("Compute Time: ", round(time.clock() - start, 3), "seconds.")
     # Animation.
     car_ani = animation.FuncAnimation(fig, update_plot, frames=range(1,len(state_i)), interval=100, repeat=True, blit=False)
-    #car_ani.save('lines.mp4')
+    #car_ani.save('mpc-video.mp4')
 
     plt.show()
