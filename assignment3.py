c0880250536c9ed225cac7f99598e86f1dc9c2a7
Daniel Stang <dstang14@gmail.com>
2018-12-15 17:35:50 -0800

    Assignment 3 ready.

---
+++ assignment3.py
@@ -0,0 +1,35 @@
+import numpy as np
+from sim.sim2d import sim_run
+
+# Simulator options.
+options = {}
+options['FIG_SIZE'] = [8,8]
+options['OBSTACLES'] = True
+
+class ModelPredictiveControl:
+    def __init__(self):
+        self.horizon = 2
+        self.dt = 0.2
+
+        # Reference or set point the controller will achieve.
+        self.reference1 = [10, 0, 0]
+        self.reference2 = None
+
+        self.x_obs = 5
+        self.y_obs = 0.1
+
+    def plant_model(self,prev_state, dt, pedal, steering):
+        x_t = prev_state[0]
+        y_t = prev_state[1]
+        psi_t = prev_state[2]
+        v_t = prev_state[3]
+
+        return [x_t, y_t, psi_t, v_t]
+
+    def cost_function(self,u, *args):
+        state = args[0]
+        ref = args[1]
+        cost = 0.0
+        return cost
+
+sim_run(options, ModelPredictiveControl)
